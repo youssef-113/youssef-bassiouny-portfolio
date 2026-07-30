2e3d6301e7f7a0eb67f8f735b6b0f90ddc78eeb0
Youssef Bassiouny Abdlah <113691068+youssef-113@users.noreply.github.com>
2026-07-30 19:08:10 +0300

    Updated the CV to be the new one

--- views/Home.tsx
+++ views/Home.tsx
@@ -123,7 +123,7 @@ const Home: React.FC = () => {
               Contact Me
             </Link>
             <a
-              href="/Youssef Bassiony - GEN AI eng & software Dev.pdf"
+              href="/Youssef Bassiony AI Engineer .pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="px-4 sm:px-6 py-2.5 sm:py-3 border border-gray-300 dark:border-gray-700 rounded-lg hover:bg-gray-100 dark:hover:bg-dark-card transition-all flex items-center justify-center gap-2 text-sm sm:text-base"
